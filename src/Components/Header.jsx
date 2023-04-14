15b950ff1754100e20d8ddd560a2e8386ca72f1e
Neoublie <dragontech.developments@gmail.com>
2023-04-14 16:27:23 +1200

    Fixed Spelling mistake

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -4,7 +4,7 @@ const Header = () => {
   return (
     <div className="header">
       <h2>PoE Jewelers Assistant</h2>
-      <h4>Asssiting you with your stats since 2023</h4>
+      <h4>Assisting you with your stats since 2023</h4>
     </div>
   );
 };
