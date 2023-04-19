a67e2bf509ef768917e2a7b8b94ba8297933df46
Neoublie <dragontech.developments@gmail.com>
2023-04-19 12:11:00 +1200

    added CodeVerifier, Removed Auth Components

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,7 +2,20 @@ import React from "react";
 
 import Header from "../Components/Header";
 import Menu from "../Components/Menu";
-import PathOfExileAuth from "../Components/PathOfExileAuth";
+
+function generateCodeVerifier() {
+  const codeVerifierLength = 64;
+  const codeVerifierCharset =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-._~";
+  let codeVerifier = "";
+
+  for (let i = 0; i < codeVerifierLength; i++) {
+    codeVerifier += codeVerifierCharset.charAt(
+      Math.floor(Math.random() * codeVerifierCharset.length)
+    );
+  }
+  return codeVerifier;
+}
 
 const Home = () => {
   return (
@@ -18,7 +31,9 @@ const Home = () => {
           <Menu />
           {/* <Characters /> */}
           {/* <StashTab /> */}
-          <PathOfExileAuth />
+          <div>
+            <p>{generateCodeVerifier()}</p>
+          </div>
         </div>
       </div>
     </>
